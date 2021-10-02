308333d385d9a0c2e706a83e14d8f297a51a3307
Luis <iluisflores@yahoo.com>
2021-10-02 16:02:21 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,66 @@
+/////////////////////
+//Dependencies
+/////////////////////
+const express = require('express');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const app = express();
+const db = mongoose.connection;
+
+
+/////////////////////
+//Port
+/////////////////////
+// Allow use of Heroku's port or your own local port, depending on the environment
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+
+/////////////////////
+//Database
+/////////////////////
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Connect to Mongo &
+// Fix Depreciation Warnings from Mongoose
+// May or may not need these depending on your Mongoose version
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Error / Success
+db.on('error', (err) => console.log(err.message + " is mongod not running?"));
+db.on('connected', () => console.log('mongod connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongod disconnected'));
+
+
+/////////////////////
+//Middleware
+/////////////////////
+
+// use publice forlder for static assets
+app.use(express.static('public'));
+
+// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
+app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
+app.use(express.json()); // returns middleware that only parses JSON - may or may not need it depending on your project
+
+// use method override
+app.use(methodOverride('_method')); // allow POST, PUT and DELETE from a form
+
+
+/////////////////////
+//Routes
+/////////////////////
+// localhost:3000
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+
+/////////////////////
+//Listener
+/////////////////////
+app.listen(PORT, () => console.log(`Express is listening on port:${PORT}`));
